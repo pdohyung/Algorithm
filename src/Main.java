be64ab2a9d5e1d64057250a05fe8924bb004a8d7
kimdohyung <pdohyung@gmail.com>
2023-06-24 23:46:22 +0900

    프로그래머스 level 2 교점에 별 만들기

---
+++ src/Main.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    private static class Point{
+        public final long x, y;
+
+        private Point(long x, long y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public String[] solution(int[][] line) {
+        List<Point> points = new ArrayList<>();
+        for (int i = 0; i < line.length; i++) {
+            for (int j = i + 1; j < line.length; j++) {
+                // line[i], line[j]를 이용하여 수행
+                Point intersection = intersection(line[i][0], line[i][1], line[i][2],
+                                                    line[j][0], line[j][1], line[j][2]);
+                if(intersection != null) {
+                    points.add(intersection);
+                }
+
+            }
+        }
+
+        Point minPoint = getMinPoint(points);
+        Point maxPoint = getMaxPoint(points);
+
+        int width =  (int) (maxPoint.x - minPoint.x + 1);
+        int height = (int) (maxPoint.y - minPoint.y + 1);
+
+        char[][] arr = new char[height][width];
+        for(char[] row : arr){
+            Arrays.fill(row, '.');
+        }
+
+        for(Point p : points){
+            int x = (int) (p.x - minPoint.x);
+            int y = (int) (maxPoint.y - p.y);
+            arr[y][x] = '*';
+        }
+
+        String[] result = new String[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            result[i] = new String(arr[i]);
+        }
+
+        return result;
+    }
+
+    private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2){
+            double x = (double) (b1 * c2  - b2 * c1) / (a1 * b2 - a2 * b1);
+            double y = (double) (a2 * c1 - a1 * c2) / (a1* b2 - a2 * b1);
+            if(x % 1 != 0 || y % 1 != 0 ) return null;
+            return new Point( (long) x, (long) y);
+    }
+
+    private Point getMinPoint(List<Point> points){
+        long x = Long.MAX_VALUE;
+        long y = Long.MAX_VALUE;
+
+        for(Point p : points){
+            if(p.x < x) x = p.x;
+            if(p.y < y) y = p.y;
+        }
+        return new Point(x, y);
+    }
+
+
+    private Point getMaxPoint(List<Point> points){
+        long x = Long.MIN_VALUE;
+        long y = Long.MIN_VALUE;
+
+        for(Point p : points){
+            if(p.x > x) x = p.x;
+            if(p.y > y) y = p.y;
+        }
+        return new Point(x, y);
+    }
+}
