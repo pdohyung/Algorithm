9c8de7a28e288a7f492f39a9f42375fb242a1d69
kimdohyung <pdohyung@gmail.com>
2023-06-28 23:55:56 +0900

    프로그래머스 level 1 시저 암호

---
+++ CaesarCipher/untitled/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public class Solution{
+        private char push(char c, int n){
+            if(!Character.isAlphabetic(c)) return c;
+
+            int offset = Character.isUpperCase(c) ? 'A' : 'a';
+            int pos = c- offset;
+            pos = (pos + n) % ('Z' - 'A' + 1);
+            return (char) (offset + pos);
+        }
+
+        public String solution(String s, int n){
+            StringBuilder builder = new StringBuilder();
+            for(char c : s.toCharArray()){
+                builder.append(push(c, n));
+            }
+            return builder.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
