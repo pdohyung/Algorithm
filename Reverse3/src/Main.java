ebae842860c5d00d90cebf5773abdca934a89487
kimdohyung <pdohyung@gmail.com>
2023-06-30 23:59:33 +0900

    프로그래머스 level 1 3진법 뒤집기

---
+++ Reverse3/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    }
+    public class Solution {
+        public int solution(int n){
+            String str = Integer.toString(n, 3);
+            String reversed = new StringBuilder(str).reverse().toString();
+            return Integer.valueOf(reversed, 3);
+        }
+    }
+}
