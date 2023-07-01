0fd39d723f4f1d23b215ef780bdc8b864bca082e
kimdohyung <pdohyung@gmail.com>
2023-07-01 23:27:29 +0900

    프로그래머스 level 1 자릿수 더하기

---
+++ PlusDigit/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+    }
+    public class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            while(true){
+                answer += n % 10;
+                if(n < 10) break;
+                n /= 10;
+            }
+            return answer;
+        }
+    }
+}
