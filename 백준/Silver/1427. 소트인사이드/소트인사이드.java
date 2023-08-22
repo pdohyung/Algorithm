bd050ee2aee315b286e1fbead9e36fa3b1d9e517
pdohyung <58456758+pdohyung@users.noreply.github.com>
2023-08-22 23:03:37 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] chars = br.readLine().toCharArray();
+        Arrays.sort(chars);
+        System.out.println(new StringBuilder(new String(chars)).reverse());
+    }
+}
