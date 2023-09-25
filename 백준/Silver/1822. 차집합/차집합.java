7094d72ef2a4c094ba103d7b9c95696527f7904e
pdohyung <58456758+pdohyung@users.noreply.github.com>
2023-09-25 17:38:06 +0900

    [Silver IV] Title: 차집합, Time: 1720 ms, Memory: 217384 KB -BaekjoonHub

---
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		TreeSet<Integer> Aset = new TreeSet<>();
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < A; i++) {
+			Aset.add(Integer.parseInt(st.nextToken()));
+		}
+		st = new StringTokenizer(br.readLine());
+		for (int j = 0; j < B; j++) {
+			int Bnum = Integer.parseInt(st.nextToken());
+			Aset.remove(Bnum);
+		}
+
+		sb.append(Aset.size() + "\n");
+		for (Integer num : Aset) {
+			sb.append(num + " ");
+		}
+		System.out.println(sb);
+	}
+}
