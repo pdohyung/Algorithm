d5c70de0163f24ddb2e483b5520de0c0dd96c0e6
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-01-07 15:10:58 +0900

    [Gold V] Title: 신기한 소수, Time: 792 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        dfs(2, 1);
+        dfs(3, 1);
+        dfs(5, 1);
+        dfs(7, 1);
+    }
+
+    private static void dfs(int num, int n) {
+        if (n == N) {
+            if (isPrimeNumber(num)) {
+                System.out.println(num);
+            }
+            return;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            if (isPrimeNumber(num * 10 + i)) {
+                dfs(num * 10 + i, n + 1);
+            }
+        }
+    }
+
+    private static boolean isPrimeNumber(int num) {
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
