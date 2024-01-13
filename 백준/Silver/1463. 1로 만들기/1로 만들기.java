b90ffd8721ffe5a205b0a131a9f1d675b22dd08d
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-01-13 19:39:38 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18152 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,19 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[] mem = new int[1000001];
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-		mem[1] = 0;
-		for(int i = 2; i <= N; i++){
-			mem[i] = mem[i - 1] + 1;
-			if(i % 2 == 0) mem[i] = Math.min(mem[i], mem[i / 2] + 1);
-			if(i % 3 == 0) mem[i] = Math.min(mem[i], mem[i / 3] + 1);
-		}
-		System.out.println(mem[N]);
-	}
+    static int[] D;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    void solution() throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        D = new int[n + 1];
+        D[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            D[i] = D[i - 1] + 1;
+            if (i % 2 == 0){
+                D[i] = Math.min(D[i], D[i / 2] + 1);
+            }
+            if (i % 3 == 0){
+                D[i] = Math.min(D[i], D[i / 3] + 1);
+            }
+        }
+        System.out.println(D[n]);
+    }
 }
