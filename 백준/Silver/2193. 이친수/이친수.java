78b72fe222637384d6e44927913cd74dd477e014
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-01-21 15:53:44 +0900

    [Silver III] Title: 이친수, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    static long[][] D;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    void solution() throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        D = new long[N + 1][2];
+
+        D[1][0] = 0;
+        D[1][1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            D[i][0] = D[i - 1][0] + D[i - 1][1];
+            D[i][1] = D[i - 1][0];
+        }
+        System.out.println(D[N][0] + D[N][1]);
+    }
+}
