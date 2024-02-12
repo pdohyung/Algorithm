68a4ee17d479c2d3a374fde0b44a957ba067e4c6
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-02-12 23:09:09 +0900

    [Silver III] Title: N과 M (6), Time: 160 ms, Memory: 15840 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    static int N, M;
+    static int[] numbers;
+    static int[] res;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    void solution() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[N];
+        res = new int[M];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+        dfs(0, 0);
+    }
+
+    private void dfs(int start, int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(res[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            res[cnt] = numbers[i];
+            dfs(i + 1, cnt + 1);
+        }
+    }
+}
