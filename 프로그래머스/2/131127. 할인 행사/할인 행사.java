597427a0577295f6d9c6c6ceddabe6f2a8d2e06f
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-06-18 23:51:28 +0900

    [level 2] Title: 할인 행사, Time: 0.06 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    
+    public int solution(String[] want, int[] number, String[] discount) {
+        HashMap<String, Integer> wantMap = new HashMap<>();
+        
+        for (int i = 0; i < want.length; i++) {
+            wantMap.put(want[i], number[i]);
+        }
+        
+        int res = 0;
+
+        for (int i = 0; i < discount.length - 9; i++) {
+            HashMap<String, Integer> discount10d = new HashMap<>();
+
+            for (int j = i; j < i + 10; j++) {
+                if (wantMap.containsKey(discount[j])) {
+                    discount10d.put(discount[j], discount10d.getOrDefault(discount[j], 0) + 1);
+                }
+            }
+            
+            if (discount10d.equals(wantMap)) {
+                res++;
+            }
+        }
+        return res;
+    }
+    
+}
