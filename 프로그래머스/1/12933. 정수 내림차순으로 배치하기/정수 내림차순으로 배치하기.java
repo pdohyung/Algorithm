d7c7032bfc586eeac6ae192a793d213bb1db104a
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-06-27 23:58:29 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.45 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        String[] digits = String.valueOf(n).split("");
+
+        Arrays.sort(digits, Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+        for (String digit : digits) {
+            sb.append(digit);
+        }
+
+        return Long.parseLong(sb.toString());
+    }
+}
