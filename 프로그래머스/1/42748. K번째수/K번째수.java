1c3078a37298a50ab9e89164e24168b2d6379494
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-06-28 19:17:11 +0900

    [level 1] Title: K번째수, Time: 0.39 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+   public int[] solution(int[] array, int[][] commands){
+       int[] answer = new int[commands.length];
+       
+       for(int c = 0; c < commands.length; c++) {
+           int i = commands[c][0];
+           int j = commands[c][1];
+           int k = commands[c][2];
+           
+           int[] slice = Arrays.copyOfRange(array, i - 1, j);
+           Arrays.sort(slice);
+           answer[c] = slice[k - 1];
+       }
+       
+       return answer;
+	}
+}
