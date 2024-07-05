51b6fb7c3042171918b25039160348769ef386ee
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-07-05 21:27:36 +0900

    [level 2] Title: [3차] n진수 게임, Time: 9.45 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -3,37 +3,25 @@ import java.io.*;
 
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        StringBuilder answer = new StringBuilder();
-        int cnt = 0;
-        int i = 0;
+        String answer = "";
+        int cur = 0;
+        int tube = p;
         
-        // for(int i = 0; i < 20; i++) {
-        //     System.out.println(Integer.toHexString(i));
-        // }
-        //for (int i = 0; i < (t * m); i++) {
-        while (true) {
-            //System.out.println(cnt);
+        for(int i = 0; i <= t * m; i++) {
             String s = Integer.toString(i, n).toUpperCase();
-            //System.out.println(s);
-            for (Character c : s.toCharArray()) {
-                //System.out.println(c);
-                if ((cnt % m) == p - 1) {
-                    answer.append(c);
-                }
-                cnt++;
+            
+            for (int j = 0; j < s.length(); j++) {
+                cur++;
                 
-                if (answer.length() == t) {
-                    break;
+                if (cur == tube) {
+                    answer += s.substring(j, j + 1);
+                    if (answer.length() == t) break;
+                    tube += m;
                 }
-            }
-            
-            i++;
-            
-            if (answer.length() == t) {
-                    break;
             }
         }
-        return answer.toString();
+        
+        return answer;
     }
     
 }
