fb68f770cb9abd522595f6391985d141c35c49e1
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-10-19 17:55:35 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,24 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
     static BufferedReader br;
     static StringTokenizer st;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
-        new Main().solution();
-    }
-
-    void solution() throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        System.out.println(fibo(N));
-    }
 
-    static long fibo(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        return fibo(n - 1) + fibo(n - 2);
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        dp = new int[N + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        System.out.println(dp[N]);
     }
 }
