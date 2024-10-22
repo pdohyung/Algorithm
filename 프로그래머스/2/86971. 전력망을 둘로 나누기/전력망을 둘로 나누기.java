a7a7e144d826bab98a3f7c7cea35ec084668f528
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-10-22 23:19:11 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.51 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,41 +2,32 @@ import java.util.*;
 
 class Solution {
     
-    // static ArrayList<Integer>[] graph;
-    static boolean[][] graph;
+    static ArrayList<Integer>[] graph;
     static boolean[] visited;
-    static int result, min, N;
+    static int result, min;
     
     public int solution(int n, int[][] wires) {
-        // graph = new ArrayList[n + 1];
-        N = n;
-        graph = new boolean[N + 1][N + 1];
+        graph = new ArrayList[n + 1];
         min = Integer.MAX_VALUE;
         
-//         // 그래프 초기화
-//         for(int i = 1; i <= n; i++) {
-//             graph[i] = new ArrayList<>();
-//         }
-        
-//         // 송전탑 전선 추가
-//         for(int[] wire : wires) {
-//             graph[wire[0]].add(wire[1]);
-//             graph[wire[1]].add(wire[0]);
-//         }
+        // 그래프 초기화
+        for(int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
         
         // 송전탑 전선 추가
         for(int[] wire : wires) {
-            graph[wire[0]][wire[1]] = true;
-            graph[wire[1]][wire[0]] = true;
+            graph[wire[0]].add(wire[1]);
+            graph[wire[1]].add(wire[0]);
         }
         
         // 완전 탐색
         for(int[] wire : wires) {
             result = 0;
             visited = new boolean[n + 1];
-            
-            graph[wire[0]][wire[1]] = false;
-            graph[wire[1]][wire[0]] = false;
+
+            graph[wire[0]].remove(Integer.valueOf(wire[1]));
+            graph[wire[1]].remove(Integer.valueOf(wire[0]));
             
             for(int i = 1; i <= n; i++) {
                 dfs(i);
@@ -45,19 +36,18 @@ class Solution {
                 }
             }
             
-            graph[wire[0]][wire[1]] = true;
-            graph[wire[1]][wire[0]] = true;
-            // System.out.println(result);
-            // System.out.println(min + " " + Math.abs(n-result));
-            min = Math.min(min, Math.abs((N - result) - result));
+            graph[wire[0]].add(wire[1]);
+            graph[wire[1]].add(wire[0]);
+            
+            min = Math.min(min, Math.abs((n - result) - result));
         }
-        
         return min;
     }
     
     public void dfs(int now) {
-        for(int next = 1; next <= N; next++) {
-            if(!visited[next] && graph[now][next]){
+        
+        for(int next : graph[now]) {
+            if(!visited[next]){
                 result++;
                 visited[next] = true;
                 dfs(next);
