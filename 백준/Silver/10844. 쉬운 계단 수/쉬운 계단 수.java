359ebe03ee294b669fd2ac6de644d0f9394b72d5
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-11-18 14:36:19 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -2,40 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
-    static long[][] D;
-    static long mod = 1_000_000_000;
+
+    static BufferedReader br;
+    static StringTokenizer st;
+    static long[][] dp;
 
     public static void main(String[] args) throws IOException {
-        new Main().solution();
-    }
+        br = new BufferedReader(new InputStreamReader(System.in));
 
-    void solution() throws IOException {
-        int N = Integer.parseInt(br.readLine());
-        D = new long[N + 1][10];
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        dp = new long[N + 1][10];
 
         for (int i = 1; i < 10; i++) {
-            D[1][i] = 1;
+            dp[1][i] = 1;
         }
 
         for (int i = 2; i <= N; i++) {
             for (int j = 0; j < 10; j++) {
-                if (j == 0) {
-                    D[i][j] = D[i - 1][j + 1];
-                    continue;
-                }
                 if (j == 9) {
-                    D[i][j] = D[i - 1][j - 1];
+                    dp[i][j] = dp[i - 1][j - 1];
                     continue;
                 }
-                D[i][j] = (D[i - 1][j - 1] + D[i - 1][j + 1]) % mod;
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][j + 1];
+                    continue;
+                }
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1_000_000_000;
             }
         }
-        long res = 0;
+
+        long answer = 0;
+
         for (int i = 0; i < 10; i++) {
-            res = (res + D[N][i]) % mod;
+            answer = (answer + dp[N][i]) % 1_000_000_000;
         }
-        System.out.println(res);
+
+        System.out.println(answer);
     }
 }
