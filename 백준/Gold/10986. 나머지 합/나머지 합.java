4ed889449da96c36ac0a5ec79e0006cb0cc8c4b4
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-11-24 15:27:18 +0900

    [Gold III] Title: 나머지 합, Time: 444 ms, Memory: 120708 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -2,42 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
-    static long[] S;
-    static long[] C;
+
+    static BufferedReader br;
+    static StringTokenizer st;
+    static long[] S, C;
 
     public static void main(String[] args) throws IOException {
-        new Main().solution();
-    }
+        br = new BufferedReader(new InputStreamReader(System.in));
 
-    void solution() throws IOException {
         st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        S = new long[n];
-        C = new long[m];
-        long ans = 0;
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        S = new long[N];
+        C = new long[M];
+        long answer = 0;
+
         st = new StringTokenizer(br.readLine());
 
         S[0] = Integer.parseInt(st.nextToken());
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < N; i++) {
             S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < n; i++) {
-            int result = (int) (S[i] % m);
+        for (int i = 0; i < N; i++) {
+            int result = (int) (S[i] % M);
             if (result == 0) {
-                ans++;
+                answer++;
             }
             C[result]++;
         }
 
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < M; i++) {
             if (C[i] > 1) {
-                ans += (C[i] * (C[i] - 1) / 2);
+                answer += (C[i] * (C[i] - 1) / 2);
             }
         }
-        System.out.println(ans);
+
+        System.out.println(answer);
     }
 }
