dfca49565d8359f1b596109b5446ec1787df5618
pdohyung <58456758+pdohyung@users.noreply.github.com>
2024-12-24 19:24:01 +0900

    [level 3] Title: 양과 늑대, Time: 3.25 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,62 +1,54 @@
 import java.util.*;
 
 class Solution {
-
-    public ArrayList<Integer>[] graph;
-    public int answer;
+    
     public int[] infoz;
+    public int[][] edgesz;
+    public int answer;
 
     public int solution(int[] info, int[][] edges) {
         infoz = info;
-
-        // 그래프 초기화
-        int length = info.length;
-        graph = new ArrayList[length];
-        for (int i = 0; i < length; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        for (int[] edge : edges) {
-            int start = edge[0];
-            int end = edge[1];
-            graph[start].add(end); // 단방향
-        }
-
+        edgesz = edges;
         answer = 0;
-
-        // 초기 상태에서 0번 노드만 방문 가능
-        ArrayList<Integer> startNodes = new ArrayList<>();
-        startNodes.add(0);
-
-        // DFS 탐색 시작
-        dfs(startNodes, 0, 0, 0);
-
+        // 시작 방문 배열 설정
+        boolean[] visited = new boolean[info.length];
+        // dfs 수행
+        dfs(visited, 0, 0, 0);
+        
         return answer;
     }
 
-    public void dfs(List<Integer> nodes, int current, int sheep, int wolf) {
-        // 현재 노드가 늑대인지 양인지 확인
-        if (infoz[current] == 0) {
+    public void dfs(boolean[] visited, int current, int sheep, int wolf) {
+        
+        // 현재 방문 노드 체크
+        visited[current] = true;
+        int type = infoz[current];
+        
+        if (type == 0) {
             sheep++;
-        } else {
+            // 양의 최댓값 갱신
+            answer = Math.max(sheep, answer);
+        } else if (type == 1) {
             wolf++;
+            // 늑대의 수가 양의 수가 같아지면 반환
+            if (wolf == sheep) {
+                return;
+            }
         }
-
-        // 늑대가 양 이상이 되면 종료
-        if (wolf >= sheep) {
-            return;
-        }
-
-        // 최대 양 갱신
-        answer = Math.max(answer, sheep);
-
-        // 현재 탐색 가능한 노드 리스트를 순회
-        List<Integer> newNodes = new ArrayList<>(nodes);
-        newNodes.remove((Integer) current); // 현재 노드 제거
-        newNodes.addAll(graph[current]); // 현재 노드의 자식 추가
-
-        for (int next : newNodes) {
-            dfs(newNodes, next, sheep, wolf);
+        
+        // 주어진 edges를 사용
+        for(int[] edge : edgesz) {
+            int start = edge[0];
+            int end = edge[1];
+            
+            // 시작 노드 방문 && 도착 노드 방문하지 않은 경우
+            if (visited[start] && !visited[end]) { 
+                boolean[] nextVisited = new boolean[visited.length];
+                for(int i = 0; i < visited.length; i++) {
+                    nextVisited[i] = visited[i];
+                }
+                dfs(nextVisited, end, sheep, wolf);
+            }
         }
     }
 }
