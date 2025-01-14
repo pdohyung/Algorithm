38c4e7f3319437b9bcf03f2f048a8d5edf2851b5
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-01-14 23:59:04 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -3,32 +3,30 @@ import java.util.*;
 
 public class Main {
 
-    static BufferedReader br;
-    static StringTokenizer st;
-    static int[] numbers, dp;
-    static int N, answer;
-
     public static void main(String[] args) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        numbers = new int[N];
-        dp = new int[N];
-        st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        int[] A = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (numbers[i] > numbers[j]) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (A[i] > A[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
 
-        for (int i = 0; i < N; i++) {
-//            System.out.println(dp[i]);
+        int answer = 0;
+
+        for (int i = 1; i <= N; i++) {
             answer = Math.max(answer, dp[i]);
         }
 
