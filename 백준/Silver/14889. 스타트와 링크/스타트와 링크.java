a720739b901353b7fb8c8a19b82989317be1eaac
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-01-17 01:38:15 +0900

    [Silver I] Title: 스타트와 링크, Time: 284 ms, Memory: 19024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,66 +1,62 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static BufferedReader br;
-    static StringTokenizer st;
     static int N, min;
     static boolean[] visited;
-    static int[][] scores;
+    static int[][] stat;
 
     public static void main(String[] args) throws IOException {
-        new Main().solution();
-    }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    void solution() throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        min = Integer.MAX_VALUE;
         visited = new boolean[N];
-        scores = new int[N][N];
+        stat = new int[N][N];
 
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                scores[i][j] = Integer.parseInt(st.nextToken());
+                stat[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        min = Integer.MAX_VALUE;
         dfs(0, 0);
+
         System.out.println(min);
     }
 
-    static int calculate() {
-        int team1 = 0;
-        int team2 = 0;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (visited[i] && visited[j]) {
-                    team1 += scores[i][j];
-                } else if (!visited[i] && !visited[j]) {
-                    team2 += scores[i][j];
-                }
-            }
-        }
-
-        return Math.abs(team1 - team2);
-    }
-
-    static void dfs(int n, int idx) {
-        if (n == N / 2) {
-            int diff = calculate();
-            min = Math.min(min, diff);
+    private static void dfs(int depth, int idx) {
+        if (depth == N / 2) {
+//            System.out.println(Arrays.toString(visited));
+            min = Math.min(cal(), min);
             return;
         }
 
         for (int i = idx; i < N; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                dfs(n + 1, i + 1);
+                dfs(depth + 1, i + 1);
                 visited[i] = false;
             }
         }
+    }
+
+    private static int cal() {
+        int team1 = 0;
+        int team2 = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (visited[i] && visited[j]) {
+                    team1 += stat[i][j];
+                } else if (!visited[i] && !visited[j]) {
+                    team2 += stat[i][j];
+                }
+            }
+        }
+
+        return Math.abs(team1 - team2);
     }
 }
