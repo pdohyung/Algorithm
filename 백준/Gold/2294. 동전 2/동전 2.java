34c9a8f325d3116134cea13284f29fbea9da3af1
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-01-31 07:48:51 +0900

    [Gold V] Title: 동전 2, Time: 116 ms, Memory: 14876 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -14,27 +14,21 @@ public class Main {
         Set<Integer> coins = new HashSet<>();
         int[] dp = new int[k + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
 
         for (int i = 0; i < n; i++) {
             int coin = Integer.parseInt(br.readLine());
             coins.add(coin);
-            if (coin <= k) {
-                dp[coin] = 1;
-            }
         }
-
 //        System.out.println(Arrays.toString(dp));
-
-        for (int i = 1; i <= k; i++) {
-            for (int coin : coins) {
-                if (i - coin > 0 && dp[i - coin] != Integer.MAX_VALUE) {
+        for (int coin : coins) {
+            for (int i = coin; i <= k; i++) {
+                if (dp[i - coin] != Integer.MAX_VALUE) {
                     dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-
 //        System.out.println(Arrays.toString(dp));
-        
         if (dp[k] == Integer.MAX_VALUE) {
             System.out.println(-1);
         } else {
