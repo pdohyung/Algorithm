1e34c47918923f3721cc20211103f610b5ecaf1b
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-02-02 00:50:52 +0900

    [Gold V] Title: 암호 만들기, Time: 124 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,13 +1,10 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
-
     static int L, C;
-    static StringBuilder answer;
     static char[] A;
-    static boolean[] visited;
+    static StringBuilder answer = new StringBuilder();
     static List<Character> vowels = List.of('a', 'e', 'i', 'o', 'u');
 
     public static void main(String[] args) throws IOException {
@@ -17,44 +14,37 @@ public class Main {
         L = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         A = new char[C];
-        visited = new boolean[C];
 
         st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < C; i++) {
             A[i] = st.nextToken().charAt(0);
         }
+
         Arrays.sort(A);
 
-        answer = new StringBuilder();
         dfs(new StringBuilder(), 0, 0, 0);
 
         System.out.println(answer);
     }
 
-    private static void dfs(StringBuilder now, int idx, int cnt1, int cnt2) {
-        if (now.length() == L && cnt1 >= 1 && cnt2 >= 2) {
-            answer.append(now).append("\n");
+    private static void dfs(StringBuilder result, int idx, int cnt1, int cnt2) {
+        if (result.length() == L) {
+            if (cnt1 >= 1 && cnt2 >= 2) {
+                answer.append(result).append("\n");
+            }
             return;
         }
 
-        for (int i = idx; i < A.length; i++) {
-            if (!visited[i]) {
-                StringBuilder next = new StringBuilder(now);
-                int newCnt1 = cnt1;
-                int newCnt2 = cnt2;
-                next.append(A[i]);
+        for (int i = idx; i < C; i++) {
+            result.append(A[i]);
 
-                if (vowels.contains(A[i])) {
-                    newCnt1++;
-                } else {
-                    newCnt2++;
-                }
-
-                visited[i] = true;
-                dfs(next, i, newCnt1, newCnt2);
-                visited[i] = false;
+            if (vowels.contains(A[i])) {
+                dfs(result, i + 1, cnt1 + 1, cnt2);
+            } else {
+                dfs(result, i + 1, cnt1, cnt2 + 1);
             }
+
+            result.deleteCharAt(result.length() - 1);
         }
     }
 }
