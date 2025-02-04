f9dd6089f32634ede662f5fd266b45aa59c84c01
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-02-04 19:09:40 +0900

    [Gold V] Title: 집합의 표현, Time: 428 ms, Memory: 50528 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,70 +1,54 @@
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
-    static int[] nodes;
+    static int[] parent;
 
     public static void main(String[] args) throws IOException {
-        new Main().solution();
-    }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    void solution() throws IOException {
-        st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        nodes = new int[n + 1];
 
-        for (int i = 1; i <= n; i++) {
-            nodes[i] = i;
-        }
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) parent[i] = i;
+
+        StringBuilder answer = new StringBuilder();
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int type = Integer.parseInt(st.nextToken());
+            int operation = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            if (type == 0) {
-                union(a, b);
-            } else {
-                if (isSame(a, b)) {
-                    System.out.println("YES");
-                    continue;
+            if (operation == 1) {
+                if (find(a) == find(b)) {
+                    answer.append("YES\n");
+                } else {
+                    answer.append("NO\n");
                 }
-                System.out.println("NO");
+            } else {
+                union(a, b);
             }
         }
+
+        System.out.println(answer);
     }
 
-    private void union(int a, int b) { // 대표 노드를 찾아서 집합을 합침
-        a = find(a);
-        b = find(b);
+    private static int find(int c) {
+        if (parent[c] == c) return c;
+        return parent[c] = find(parent[c]);
+    }
 
-        if (a != b) {
-            nodes[b] = a;
+    private static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if (rootA != rootB) {
+            parent[rootB] = rootA;
         }
     }
-
-    private int find(int n) { // 대표 노드 찾기
-        if (nodes[n] == n) {
-            return n;
-        } else {
-            return nodes[n] = find(nodes[n]);
-        }
-    }
-
-    private boolean isSame(int a, int b) {
-        a = find(a);
-        b = find(b);
-
-        if (a == b) {
-            return true;
-        }
-
-        return false;
-    }
-
 }
