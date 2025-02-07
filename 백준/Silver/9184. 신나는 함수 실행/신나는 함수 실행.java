168d5f70b8ee278131dc95c1bce4ad98e9062333
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-02-07 21:44:37 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 252 ms, Memory: 26832 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -3,58 +3,38 @@ import java.io.*;
 
 public class Main {
 
-    static BufferedReader br;
-    static StringTokenizer st;
-    static int N;
-    static int[][][] dp;
-    static boolean[][][] visited;
+    static int[][][] dp = new int[21][21][21];
 
     public static void main(String[] args) throws IOException {
-        new Main().solution();
-    }
-
-    void solution() throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        dp = new int[101][101][101];
-        visited = new boolean[101][101][101];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder answer = new StringBuilder();
 
         while (true) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            if (a == -1 && b == -1 && c == -1) {
-                break;
-            }
+            if (a == -1 && b == -1 && c == -1) break;
 
-            System.out.println("w(" + a + ", " + b + ", " + c + ") = " + w(a, b, c));
+            answer.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ").append(w(a, b, c)).append("\n");
         }
 
+        System.out.println(answer);
     }
 
-    static int w(int a, int b, int c) {
-        int aa = a + 50;
-        int bb = b + 50;
-        int cc = c + 50;
+    private static int w(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) return 1;
+        if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
 
-        if (visited[aa][bb][cc]) {
-            return dp[aa][bb][cc];
-        }
+        if (dp[a][b][c] != 0) return dp[a][b][c];
 
-        if (a <= 0 || b <= 0 || c <= 0) {
-            visited[aa][bb][cc] = true;
-            return dp[aa][bb][cc] = 1;
-        } else if (a > 20 || b > 20 || c > 20) {
-            visited[aa][bb][cc] = true;
-            return dp[aa][bb][cc] = w(20, 20, 20);
-        } else if (a < b && b < c) {
-            visited[aa][bb][cc] = true;
-            return dp[aa][bb][cc] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        if (a < b && b < c) {
+            dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
         } else {
-            visited[aa][bb][cc] = true;
-            return dp[aa][bb][cc] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+            dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
         }
-    }
 
+        return dp[a][b][c];
+    }
 }
