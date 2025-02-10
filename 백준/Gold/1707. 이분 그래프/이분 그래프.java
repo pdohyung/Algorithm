6574a0eac074b1049070b3c78bb51170b110938c
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-02-10 21:12:40 +0900

    [Gold IV] Title: 이분 그래프, Time: 876 ms, Memory: 269612 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,72 +1,62 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
+    static int v, e;
     static ArrayList<Integer>[] graph;
-    static boolean[] visit;
-    static int[] group;
-    static boolean flag;
+    static int[] color;
+    static boolean isCycle;
 
     public static void main(String[] args) throws IOException {
-        new Main().solution();
-    }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    void solution() throws IOException {
-        int K = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder answer = new StringBuilder();
 
-        for (int i = 0; i < K; i++) {
-            st = new StringTokenizer(br.readLine());
-            int V = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
-            graph = new ArrayList[V + 1];
-            visit = new boolean[V + 1];
-            group = new int[V + 1];
-            flag = false;
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            for (int j = 1; j <= V; j++) {
-                graph[j] = new ArrayList<>();
-            }
+            v = Integer.parseInt(st.nextToken());
+            e = Integer.parseInt(st.nextToken());
 
-            for (int j = 0; j < E; j++) {
+            graph = new ArrayList[v + 1];
+            for (int i = 1; i <= v; i++) graph[i] = new ArrayList<>();
+
+            for (int i = 0; i < e; i++) {
                 st = new StringTokenizer(br.readLine());
                 int start = Integer.parseInt(st.nextToken());
                 int end = Integer.parseInt(st.nextToken());
-
                 graph[start].add(end);
                 graph[end].add(start);
             }
 
-            for (int j = 1; j <= V; j++) {
-                dfs(j);
+            isCycle = false;
+            color = new int[v + 1];
+
+            for (int i = 1; i <= v; i++) {
+                if (color[i] != 0) continue;
+                dfs(i, 1);
             }
 
-            if (flag) {
-                System.out.println("NO");
-                continue;
-            }
-            System.out.println("YES");
-        }
-
-    }
-
-    private void dfs(int startV) {
-        visit[startV] = true;
-
-        for (int v : graph[startV]) {
-            if (!visit[v]) {
-                group[v] = (group[startV] + 1) % 2;
-                dfs(v);
-                continue;
-            }
-
-            if (group[startV] == group[v]) {
-                flag = true;
-                return;
+            if (isCycle) {
+                answer.append("NO\n");
+            } else {
+                answer.append("YES\n");
             }
         }
+
+        System.out.println(answer);
     }
 
+    private static void dfs(int start, int c) {
+        color[start] = c;
+
+        for (int next : graph[start]) {
+            if (color[next] == 0) {
+                dfs(next, -c);
+            } else if (color[start] == color[next]) {
+                isCycle = true;
+            }
+        }
+    }
 }
