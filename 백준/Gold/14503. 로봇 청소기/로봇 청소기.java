e015d4eb036936b4e8b1c9b2a26d8e540753f6e2
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-02-11 00:35:06 +0900

    [Gold V] Title: 로봇 청소기, Time: 112 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,29 +1,38 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
+class Node {
+    int x;
+    int y;
+    int d;
+
+    public Node(int x, int y, int d) {
+        this.x = x;
+        this.y = y;
+        this.d = d;
+    }
+}
 
 public class Main {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
-    static int N, M, r, c, d, cnt;
+
+    static int N, M, r, c, d, answer;
     static int[][] map;
-    static boolean[][] visit;
-    static int[] dx = {-1, 0, 1, 0};
+    static int[] dx = {-1, 0, 1, 0}; // 북 동 남 서
     static int[] dy = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
-        new Main().solution();
-    }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    void solution() throws IOException {
-        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+
         st = new StringTokenizer(br.readLine());
+
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
         d = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        visit = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -32,32 +41,58 @@ public class Main {
             }
         }
 
-        cnt = 1;
-        bfs(r, c, d);
+        bfs(new Node(r, c, d));
 
-        System.out.println(cnt);
+        System.out.println(answer);
     }
 
-    private void bfs(int r, int c, int d) {
-        visit[r][c] = true;
+    private static void bfs(Node start) {
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(start);
 
-        for (int i = 0; i < 4; i++) {
-            d = (d + 3) % 4;
-            int nx = r + dx[d];
-            int ny = c + dy[d];
+        while (!q.isEmpty()) {
+            Node now = q.poll();
+            int x = now.x;
+            int y = now.y;
+            int dir = now.d;
 
-            if (ny < 0 || ny >= M || nx < 0 || nx >= N) continue;
-            if (visit[nx][ny] || map[nx][ny] != 0) continue;
+            if (map[x][y] == 0) {
+                answer++;
+                map[x][y] = -1;
+            }
 
-            cnt++;
-            bfs(nx, ny, d);
-            return;
+            boolean flag = true;
+            int cnt = 0;
+            int nextDir = dir;
+            
+            // 바라보는 방향에서부터 진행
+            while (cnt < 4) {
+                cnt++;
+                nextDir = (nextDir + 3) % 4;
+
+                int nx = x + dx[nextDir];
+                int ny = y + dy[nextDir];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || map[nx][ny] == 1 || map[nx][ny] == -1) continue;
+
+                flag = false;
+                map[nx][ny] = -1;
+                answer++;
+                q.add(new Node(nx, ny, nextDir));
+                break;
+            }
+
+            // 청소되지 않은 빈칸이 없는 경우
+            if (flag) {
+                nextDir = (dir + 2) % 4;
+                int nx = x + dx[nextDir];
+                int ny = y + dy[nextDir];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || map[nx][ny] == 1) return;
+                else {
+                    q.add(new Node(nx, ny, dir));
+                }
+            }
         }
-
-        int nowDir = (d + 2) % 4;
-        int nx = r + dx[nowDir];
-        int ny = c + dy[nowDir];
-        if (ny < 0 || ny >= M || nx < 0 || nx >= N || map[nx][ny] == 1) return;
-        bfs(nx, ny, d);
     }
 }
