38d888bf93d2b1c2be68d126862e394ade1bf054
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-02-12 00:35:04 +0900

    [Silver II] Title: 연속합, Time: 228 ms, Memory: 24552 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -6,24 +6,27 @@ public class Main {
     static BufferedReader br;
     static StringTokenizer st;
     static int[] dp, num;
-    static int N, max;
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
         dp = new int[N + 1];
         num = new int[N + 1];
-        st = new StringTokenizer(br.readLine());
 
         for (int i = 1; i <= N; i++) {
             num[i] = Integer.parseInt(st.nextToken());
         }
-        
-        max = num[1];
-        
+
         for (int i = 1; i <= N; i++) {
             dp[i] = Math.max(dp[i - 1] + num[i], num[i]);
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 1; i <= N; i++) {
             max = Math.max(max, dp[i]);
         }
 
