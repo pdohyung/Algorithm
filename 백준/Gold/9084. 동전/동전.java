9731574c342c7a877c7bef318fcbb1af6908c556
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-02-15 20:01:47 +0900

    [Gold V] Title: 동전, Time: 108 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.java
+++ 백준/Gold/9084. 동전/동전.java
@@ -12,28 +12,20 @@ public class Main {
 
         for (int t = 0; t < T; t++) {
             int N = Integer.parseInt(br.readLine());
-
             int[] A = new int[N];
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) A[i] = Integer.parseInt(st.nextToken());
 
             int target = Integer.parseInt(br.readLine());
             int[] dp = new int[target + 1];
-//            dp[0] = 1;
+            dp[0] = 1;
 
             for (int i = 0; i < N; i++) {
                 int now = A[i];
-
-                if (now <= target) dp[now] += 1;
-
                 for (int j = now; j <= target; j++) {
-//                    System.out.println(j + " " + dp[j - now]);
                     dp[j] += dp[j - now];
                 }
             }
-
-//            System.out.println(Arrays.toString(dp));
-//            System.out.println(dp[target]);
             answer.append(dp[target]).append("\n");
         }
 
