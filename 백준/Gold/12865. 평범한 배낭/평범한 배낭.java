5587da827e5ffadbc937da8352bc39a67f4b9324
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-03-21 20:55:32 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 14700 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,30 +1,28 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-
-        int[][] things = new int[N][2];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            things[i][0] = Integer.parseInt(st.nextToken());
-            things[i][1] = Integer.parseInt(st.nextToken());
-        }
-
+        int[] W = new int[N];
+        int[] V = new int[N];
         int[] dp = new int[K + 1];
 
         for (int i = 0; i < N; i++) {
-            int w = things[i][0];
-            int v = things[i][1];
+            st = new StringTokenizer(br.readLine());
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
+        }
 
-            for (int j = K; j >= w; j--) {
-                dp[j] = Math.max(dp[j], dp[j - w] + v);
+        for (int i = 0; i < N; i++) {
+            for (int j = K; j >= W[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - W[i]] + V[i]);
             }
         }
 
