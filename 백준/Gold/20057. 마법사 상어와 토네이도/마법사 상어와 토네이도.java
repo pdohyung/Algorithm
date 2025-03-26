eab34cfbc0a084f5d14ea12c97b739bf6c353709
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-03-26 11:19:34 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 432 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
@@ -1,93 +1,105 @@
+
 import java.util.*;
 import java.io.*;
 
+class Node {
+    int x, y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class Main {
 
-    static BufferedReader br;
-    static StringTokenizer st;
-    static int N, result;
+    static int N, dir, answer;
     static int[][] map;
+    static Node start;
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {-1, 0, 1, 0};
-    static int[][] sandsX = {
-            {0, -1, 1, -1, 1, -2, 2, -1, 1},
-            {2, 1, 1, 0, 0, 0, 0, -1, -1},
-            {0, -1, 1, -1, 1, -2, 2, -1, 1},
-            {-2, -1, -1, 0, 0, 0, 0, 1, 1}
+    static int[][] sandXDirs = {
+            {0, 1, 1, 2, 1, -1, -1, -2, -1},
+            {2, 1, 0, 0, -1, 1, 0, 0, -1},
+            {0, -1, -1, -2, -1, 1, 1, 2, 1},
+            {-2, -1, 0, 0, 1, -1, 0, 0, 1}
     };
-    static int[][] sandsY = {
-            {-2, -1, -1, 0, 0, 0, 0, 1, 1},
-            {0, -1, 1, -1, 1, -2, 2, -1, 1},
-            {2, 1, 1, 0, 0, 0, 0, -1, -1},
-            {0, -1, 1, -1, 1, -2, 2, -1, 1}
+    static int[][] sandYDirs = {
+            {-2, -1, 0, 0, 1, -1, 0, 0, 1},
+            {0, 1, 1, 2, 1, -1, -1, -2, -1},
+            {2, 1, 0, 0, -1, 1, 0, 0, -1},
+            {0, -1, -1, -2, -1, 1, 1, 2, 1}
     };
-    static double[] sandsPer = {0.05, 0.1, 0.1, 0.07, 0.07, 0.02, 0.02, 0.01, 0.01};
+    static int[] percent = {5, 10, 7, 2, 1, 10, 7, 2, 1};
 
     public static void main(String[] args) throws IOException {
-        new Main().solution();
-    }
-
-    void solution() throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
 
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) map[i][j] = Integer.parseInt(st.nextToken());
         }
 
-        int dir = 0;
-        int x = N / 2;
-        int y = N / 2;
+        // 토네이도 시작
+        start = new Node(N / 2, N / 2);
+        dir = -1;
+        tornado();
 
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < 2; j++) {
-                for (int k = 0; k < i; k++) {
-                    x += dx[dir];
-                    y += dy[dir];
-                    if (x < 0 || y < 0 || x >= N || y >= N) break;
-                    wind(x, y, dir);
-                }
-                dir = (dir + 1) % 4;
-            }
-        }
-
-        for (int i = 0; i < N - 1; i++) {
-            x += dx[dir];
-            y += dy[dir];
-            wind(x, y, dir);
-        }
-
-        System.out.println(result);
+        System.out.println(answer);
     }
 
-    static void wind(int x, int y, int dir) {
-        int total = 0;
+    private static void tornado() {
+        // 중간부터 (0,0)까지 수행
+        for (int i = 1; i < N; i++) {
+            // 마지막은 3번 수행
+            int cnt = 2;
+            if (i == N - 1) cnt = 3;
+            phase(cnt, i);
+        }
+    }
 
-        for (int k = 0; k < 9; k++) {
-            int nx = x + sandsX[dir][k];
-            int ny = y + sandsY[dir][k];
-            int sand = (int) (map[x][y] * sandsPer[k]);
-            total += sand;
-            if (ny >= N || ny < 0 || nx >= N || nx < 0) {
-                result += sand;
-            } else {
-                map[nx][ny] += sand;
+    private static void phase(int cnt, int n) {
+        for (int i = 0; i < cnt; i++) {
+            dir = (dir + 1) % 4; // dir 수정
+            int x = start.x;
+            int y = start.y;
+            for (int j = 0; j < n; j++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                calculateSand(nx, ny);
+                x = nx;
+                y = ny;
             }
+            // 시작 지점 업데이트
+            start = new Node(x, y);
         }
+    }
 
-        int remainingSandX = x + dx[dir];
-        int remainingSandY = y + dy[dir];
-        int remainingSand = map[x][y] - total;
-        if (remainingSandY >= N || remainingSandY < 0 || remainingSandX >= N || remainingSandX < 0) {
-            result += remainingSand;
-        } else {
-            map[remainingSandX][remainingSandY] += remainingSand;
-        }
-
+    private static void calculateSand(int x, int y) {
+        int total = map[x][y];
+        int remain = map[x][y];
         map[x][y] = 0;
+        int[] xDirs = sandXDirs[dir];
+        int[] yDirs = sandYDirs[dir];
+
+        // 모래를 위치별로 저장
+        for (int i = 0; i < xDirs.length; i++) {
+            int nx = x + xDirs[i];
+            int ny = y + yDirs[i];
+
+            int sand = total * percent[i] / 100;
+            remain -= sand;
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N) answer += sand;
+            else map[nx][ny] += sand;
+        }
+
+        // 나머지 모래를 저장
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        if (nx < 0 || nx >= N || ny < 0 || ny >= N) answer += remain;
+        else map[nx][ny] += remain;
     }
 }
