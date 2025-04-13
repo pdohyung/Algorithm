cf3cf970fb8c4820262ef3583f349a3fe026872a
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-04-13 18:25:50 +0900

    [Silver II] Title: 좌표 압축, Time: 1724 ms, Memory: 264860 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -6,21 +6,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Integer> original = new ArrayList<>();
-        for (int i = 0; i < N; i++) original.add(Integer.parseInt(st.nextToken()));
 
-        Map<Integer, Integer> map = new HashMap<>();
-        Set<Integer> set = new TreeSet<>(original);
-        Stack<Integer> stack = new Stack<>();
-        for (int n : set) stack.push(n);
+        Map<Integer, Integer> ranking = new HashMap<>();
+        int[] origin = new int[N];
+        int[] sorted = new int[N];
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            origin[i] = n;
+            sorted[i] = n;
+        }
 
-        while (!stack.isEmpty()) {
-            int n = stack.pop();
-            map.put(n, stack.size());
+        Arrays.sort(sorted);
+
+        int rank = 0;
+        for (int i = 0; i < N; i++) {
+            int now = sorted[i];
+
+            if (!ranking.containsKey(now)) {
+                ranking.put(now, rank);
+                rank++;
+            }
         }
 
         StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < N; i++) answer.append(map.get(original.get(i))).append(" ");
+        for (int i = 0; i < N; i++) answer.append(ranking.get(origin[i])).append(" ");
         System.out.println(answer);
     }
 }
