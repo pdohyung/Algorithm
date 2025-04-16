da12ca894c64dc5dbd7925d07a01d07d6a206003
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-04-16 10:10:18 +0900

    [level 3] Title: 기지국 설치, Time: 0.64 ms, Memory: 55.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        int before = 0;
+        int W = (w * 2) + 1;
+        
+        for (int station : stations) {
+            int start = station - w;
+            int end = station + w;
+            
+            if (start < 1 || start <= before) {
+                before = end;
+                continue;
+            }
+            
+            int now = start - before - 1;
+            answer += now / W;
+            if (now % W != 0) answer++;
+            
+            before = end;
+        }
+
+        if (before < n) {
+            int now = n - before;
+            answer += now / W;
+            if (now % W != 0) answer++;
+        }
+        
+        return answer;
+    }
+}
