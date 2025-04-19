4a6f52a196943cc129be42e2866d7f7b22f08b78
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-04-19 16:27:43 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,33 +1,31 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int[][] cost = new int[1002][4];
-	static int[] R = new int[1002];
-	static int[] G = new int[1002];
-	static int[] B = new int[1002];
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] A = new int[N + 1][3];
+        int[][] dp = new int[N + 1][3];
 
-		for(int i = 1; i <= N; i++){
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			R[i] = Integer.parseInt(st.nextToken());
-			G[i] = Integer.parseInt(st.nextToken());
-			B[i] = Integer.parseInt(st.nextToken());
-		}
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            A[i][0] = Integer.parseInt(st.nextToken());
+            A[i][1] = Integer.parseInt(st.nextToken());
+            A[i][2] = Integer.parseInt(st.nextToken());
+        }
 
-		cost[1][0] = R[1];
-		cost[1][1] = G[1];
-		cost[1][2] = B[1];
+        for (int i = 1; i <= N; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + A[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + A[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + A[i][2];
+        }
 
-		for(int i = 2; i<= N; i++){
-			cost[i][0] = Math.min(cost[i - 1][1], cost[i - 1][2]) + R[i];
-			cost[i][1] = Math.min(cost[i - 1][0], cost[i - 1][2]) + G[i];
-			cost[i][2] = Math.min(cost[i - 1][0], cost[i - 1][1]) + B[i];
-		}
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            answer = Math.min(answer, dp[N][i]);
+        }
 
-		System.out.println(Math.min(Math.min(cost[N][0], cost[N][1]), cost[N][2]));
-	}
+        System.out.println(answer);
+    }
 }
