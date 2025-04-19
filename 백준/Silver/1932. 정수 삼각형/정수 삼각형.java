c9a07c46f21cf74c3d00148dd38c6c4c71d88667
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-04-19 17:07:52 +0900

    [Silver I] Title: 정수 삼각형, Time: 248 ms, Memory: 25400 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,38 +1,25 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    static int N;
-    static int[][] tree;
-    static int[][] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N + 1][N + 1];
 
-        N = Integer.parseInt(br.readLine());
-
-        tree = new int[N + 1][N + 1];
-        dp = new int[N + 1][N + 1];
-
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j <= i; j++) {
-                tree[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-//        for (int i = 0; i < N; i++) {
-//            System.out.println(Arrays.toString(tree[i]));
-//        }
-
-        for (int i = N - 2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                tree[i][j] += Math.max(tree[i + 1][j], tree[i + 1][j + 1]);
+        for (int i = N - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + dp[i][j];
             }
         }
 
-        System.out.println(tree[0][0]);
+        System.out.println(dp[1][1]);
     }
 }
