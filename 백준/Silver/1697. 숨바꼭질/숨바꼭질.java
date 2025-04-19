94287fbd45645eef3bfa48220abc2f851a44f36b
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-04-20 00:58:34 +0900

    [Silver I] Title: 숨바꼭질, Time: 184 ms, Memory: 17984 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -5,7 +5,7 @@ import java.io.*;
 class Node {
     int pos, cnt;
 
-    public Node(int pos, int cnt) {
+    Node(int pos, int cnt) {
         this.pos = pos;
         this.cnt = cnt;
     }
@@ -16,15 +16,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int max = Math.max(N, K);
 
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cnt, o2.cnt));
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.cnt, b.cnt));
         pq.add(new Node(N, 0));
-
-        int[] dist = new int[2 * max + 1];
+        int[] dist = new int[100_001];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[N] = 0;
 
@@ -33,22 +30,21 @@ public class Main {
             int pos = now.pos;
             int cnt = now.cnt;
 
-            if (pos == K) break;
             if (dist[pos] < cnt) continue;
 
-            if (pos + 1 < dist.length && dist[pos + 1] > cnt + 1) {
-                dist[pos + 1] = cnt + 1;
-                pq.add(new Node(pos + 1, cnt + 1));
+            if (2 * pos <= 100_000 && dist[2 * pos] > dist[pos] + 1) {
+                dist[2 * pos] = dist[pos] + 1;
+                pq.add(new Node(2 * pos, cnt + 1));
             }
 
-            if (pos - 1 >= 0 && dist[pos - 1] > cnt + 1) {
-                dist[pos - 1] = cnt + 1;
+            if (pos - 1 >= 0 && dist[pos - 1] > dist[pos] + 1) {
+                dist[pos - 1] = dist[pos] + 1;
                 pq.add(new Node(pos - 1, cnt + 1));
             }
 
-            if (pos * 2 < dist.length && dist[pos * 2] > cnt + 1) {
-                dist[pos * 2] = cnt + 1;
-                pq.add(new Node(pos * 2, cnt + 1));
+            if (pos + 1 <= 100_000 && dist[pos + 1] > dist[pos] + 1) {
+                dist[pos + 1] = dist[pos] + 1;
+                pq.add(new Node(pos + 1, cnt + 1));
             }
         }
 
