2e0e9a9676b393f03d08653468c76a0e54313144
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-04-19 18:21:51 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18316 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,28 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
 
-    static int[] D;
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
-        new Main().solution();
-    }
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-    void solution() throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        D = new int[n + 1];
-        D[1] = 0;
-        for (int i = 2; i <= n; i++) {
-            D[i] = D[i - 1] + 1;
-            if (i % 2 == 0){
-                D[i] = Math.min(D[i], D[i / 2] + 1);
-            }
-            if (i % 3 == 0){
-                D[i] = Math.min(D[i], D[i / 3] + 1);
-            }
+        dp = new int[N + 1];
+
+        dp[1] = 0;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
         }
-        System.out.println(D[n]);
+
+        System.out.println(dp[N]);
     }
 }
