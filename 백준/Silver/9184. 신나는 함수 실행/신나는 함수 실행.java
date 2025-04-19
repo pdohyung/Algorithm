4e57138bf4766bd539dcb4f5112fcf426541e22e
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-04-19 15:52:05 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 248 ms, Memory: 26648 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -3,34 +3,39 @@ import java.io.*;
 
 public class Main {
 
-    static int[][][] dp = new int[21][21][21];
+    static int[][][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        dp = new int[21][21][21];
         StringBuilder answer = new StringBuilder();
 
-        while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+        w(20, 20, 20);
 
-            if (a == -1 && b == -1 && c == -1) break;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
+        while (!(a == -1 && b == -1 && c == -1)) {
             answer.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ").append(w(a, b, c)).append("\n");
+
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
         }
 
         System.out.println(answer);
     }
 
-    private static int w(int a, int b, int c) {
+    static int w(int a, int b, int c) {
         if (a <= 0 || b <= 0 || c <= 0) return 1;
-        if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
-
+        if (a > 20 || b > 20 || c > 20) return dp[20][20][20];
         if (dp[a][b][c] != 0) return dp[a][b][c];
 
         if (a < b && b < c) {
-            dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+            dp[a][b][c] = w(a, b, c - 1) + w(a, b, c - 1) - w(a, b - 1, c);
         } else {
             dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
         }
