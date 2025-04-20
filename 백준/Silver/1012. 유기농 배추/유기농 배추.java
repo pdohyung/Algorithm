2a2b8f659a2f929603dabb157d7b13057bbc6247
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-04-20 14:24:13 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 15908 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,82 @@
+
+import java.util.*;
+import java.io.*;
+
+class Node {
+    int x, y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+
+    static int N, M, K;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder answer = new StringBuilder();
+
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            map = new int[N][M];
+            visited = new boolean[N][M];
+
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int y = Integer.parseInt(st.nextToken());
+                int x = Integer.parseInt(st.nextToken());
+                map[x][y] = 1;
+            }
+
+            int cnt = 0;
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (!visited[i][j] && map[i][j] == 1) {
+                        bfs(new Node(i, j));
+                        cnt++;
+                    }
+                }
+            }
+
+            answer.append(cnt).append("\n");
+        }
+
+        System.out.println(answer);
+    }
+
+    static void bfs(Node start) {
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(start);
+        visited[start.x][start.y] = true;
+
+        while (!q.isEmpty()) {
+            Node now = q.poll();
+            int x = now.x;
+            int y = now.y;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                if (visited[nx][ny] || map[nx][ny] == 0) continue;
+
+                visited[nx][ny] = true;
+                q.add(new Node(nx, ny));
+            }
+        }
+    }
+}
