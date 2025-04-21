bf1de1ca42d74b920c83416d3d92f125c22a1579
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-04-21 10:48:25 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 132 ms, Memory: 15540 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,45 +1,44 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
         int[] A = new int[N];
+        int[] dp1 = new int[N];
+        int[] dp2 = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dpIn = new int[N];
-        int[] dpDe = new int[N];
-        Arrays.fill(dpIn, 1);
-        Arrays.fill(dpDe, 1);
+        Arrays.fill(dp1, 1);
+        Arrays.fill(dp2, 1);
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < i; j++) {
-                if (A[j] < A[i]) {
-                    dpIn[i] = Math.max(dpIn[i], dpIn[j] + 1);
+                if (A[i] > A[j]) {
+                    dp1[i] = Math.max(dp1[i], dp1[j] + 1);
                 }
             }
         }
 
         for (int i = N - 1; i >= 0; i--) {
             for (int j = N - 1; j > i; j--) {
-                if (A[j] < A[i]) {
-                    dpDe[i] = Math.max(dpDe[i], dpDe[j] + 1);
+                if (A[i] > A[j]) {
+                    dp2[i] = Math.max(dp2[i], dp2[j] + 1);
                 }
             }
         }
-//        System.out.println(Arrays.toString(dpIn));
-//        System.out.println(Arrays.toString(dpDe));
 
         int answer = 0;
-
         for (int i = 0; i < N; i++) {
-            answer = Math.max(answer, dpIn[i] + dpDe[i] - 1);
+            answer = Math.max(answer, dp1[i] + dp2[i] - 1);
         }
 
         System.out.println(answer);
