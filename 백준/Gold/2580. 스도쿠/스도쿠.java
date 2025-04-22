123ec7b3e1f509b131eada091b88cacf8f68b43c
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-04-22 16:38:38 +0900

    [Gold IV] Title: 스도쿠, Time: 392 ms, Memory: 21300 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,10 +1,9 @@
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Node {
-    int x;
-    int y;
+    int x, y;
 
     public Node(int x, int y) {
         this.x = x;
@@ -13,81 +12,77 @@ class Node {
 }
 
 public class Main {
-
     static int[][] board;
-    static ArrayDeque<Node> targets;
+    static List<Node> zero;
+    static boolean flag;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        board = new int[10][10];
-        targets = new ArrayDeque<>();
+        board = new int[9][9];
+        zero = new ArrayList<>();
 
         for (int i = 0; i < 9; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+
             for (int j = 0; j < 9; j++) {
-                int num = Integer.parseInt(st.nextToken());
+                int now = Integer.parseInt(st.nextToken());
+                if (now == 0) zero.add(new Node(i, j));
+                board[i][j] = now;
+            }
+        }
 
-                if (num == 0) {
-                    targets.push(new Node(i, j));
+        flag = false;
+        dfs(0);
+    }
+
+    static void dfs(int depth) {
+        if (flag) return;
+        if (depth == zero.size()) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    System.out.print(board[i][j] + " ");
                 }
+                System.out.println();
+            }
 
-                board[i][j] = num;
+            flag = true;
+            return;
+        }
+
+        Node z = zero.get(depth);
+        int x = z.x;
+        int y = z.y;
+
+        for (int n = 1; n <= 9; n++) {
+            if (isValid(x, y, n)) {
+                board[x][y] = n;
+                dfs(depth + 1);
+                if (flag) return;
+                board[x][y] = 0;
             }
         }
-
-        dfs();
     }
 
-    private static void dfs() {
-        if (targets.isEmpty()) {
-            printBoard(); // 출력
-            System.exit(0);
-        }
-
-        Node target = targets.pop();
-
-        for (int i = 1; i <= 9; i++) {
-            if (isValid(i, target)) {
-                board[target.x][target.y] = i;
-                dfs(); // 다음 칸으로 이동
-                board[target.x][target.y] = 0;
-            }
-        }
-
-        targets.push(target);
-    }
-
-    private static boolean isValid(int num, Node target) {
-        int row = target.x;
-        int col = target.y;
+    static boolean isValid(int x, int y, int n) {
 
         for (int i = 0; i < 9; i++) {
-            if (board[row][i] == num) return false;
+            if (board[x][i] == n) return false;
         }
 
         for (int i = 0; i < 9; i++) {
-            if (board[i][col] == num) return false;
+            if (board[i][y] == n) return false;
         }
 
-        int startRow = (row / 3) * 3;
-        int startCol = (col / 3) * 3;
+        int nx = (x / 3) * 3;
+        int ny = (y / 3) * 3;
 
-        for (int i = startRow; i < startRow + 3; i++) {
-            for (int j = startCol; j < startCol + 3; j++) {
-                if (board[i][j] == num) return false;
+        for (int i = nx; i < nx + 3; i++) {
+            for (int j = ny; j < ny + 3; j++) {
+                if (board[i][j] == n) return false;
             }
         }
 
         return true;
-    }
-
-    private static void printBoard() {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
     }
 }
