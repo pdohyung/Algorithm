e4d268cb15d86d99bb8bfe78a94bf28c387f7cef
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-04-23 17:49:48 +0900

    [level 4] Title: 징검다리, Time: 30.61 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
         Arrays.sort(rocks);
-        int len = rocks.length;
+        
         int start = 1;
         int end = distance;
         
@@ -12,7 +12,7 @@ class Solution {
             int before = 0;
             int cnt = 0;
             
-            for (int i = 0; i < len; i++) {
+            for (int i = 0; i < rocks.length; i++) {
                 if (rocks[i] - before < mid) {
                     cnt++;
                 } else {
@@ -22,13 +22,14 @@ class Solution {
             
             if (distance - before < mid) cnt++;
             
-            if (cnt > n) {
-                end = mid - 1;
-            } else {
+            if (cnt <= n) {
                 start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         
         return end;
     }
 }
+
