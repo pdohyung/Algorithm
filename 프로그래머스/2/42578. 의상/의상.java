b7d96ca14e4abc8a0b58f6b60ddc773ec584e11f
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-04-24 13:55:00 +0900

    [level 2] Title: 의상, Time: 0.18 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,16 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> types = new HashMap<>();
         
-        for (String[] s : clothes) {
-            String name = s[0];
-            String type = s[1];
-            map.put(type, map.getOrDefault(type, 0) + 1);
+        for (String[] c : clothes) {
+            types.put(c[1], types.getOrDefault(c[1], 0) + 1);
         }
         
         int answer = 1;
-        for (String key : map.keySet()) answer *= map.get(key) + 1;
+        
+        for (String key : types.keySet()) {
+            answer *= types.get(key) + 1;
+        }
         
         return answer - 1;
     }
