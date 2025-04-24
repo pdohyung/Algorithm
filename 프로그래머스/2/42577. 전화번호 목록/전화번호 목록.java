be524853aa7fe0039228f6380351d4148effe78e
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-04-24 16:37:03 +0900

    [level 2] Title: 전화번호 목록, Time: 549.61 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,18 +1,17 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
-        //System.out.println(Arrays.toString(phone_book));
         
-        for(int i = 0; i < phone_book.length - 1; i++) {
-            String pre = phone_book[i];
-            String next = phone_book[i + 1];
-            
-            if(next.startsWith(pre)) {
+        String before = phone_book[0];
+        
+        for (int i = 1; i < phone_book.length; i++) {
+            if (phone_book[i].startsWith(before)) {
                 return false;
             }
+            
+            before = phone_book[i];
         }
         
         return true;
