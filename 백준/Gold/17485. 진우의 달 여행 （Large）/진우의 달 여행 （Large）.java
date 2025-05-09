5d83ba88e670b8091a2f6e108e3e5e10ba17ebbf
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-05-09 20:17:25 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 560 ms, Memory: 111204 KB -BaekjoonHub

--- 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.java
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.java
@@ -7,44 +7,45 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        int[][] cost = new int[N + 1][M + 1];
 
-        int[][] cost = new int[N][M];
-
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) cost[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= M; j++) {
+                cost[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        int[][][] dp = new int[N][M][3];
-        int[] dir = {-1, 0, 1};
+        int[][][] dp = new int[N + 1][M + 1][3];
 
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < M; j++)
-                Arrays.fill(dp[i][j], Integer.MAX_VALUE);
-
-        for (int i = 0; i < M; i++) Arrays.fill(dp[0][i], cost[0][i]);
-
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                for (int d = 0; d < 3; d++) {
-                    int beforeCol = j + dir[d];
-                    if (beforeCol < 0 || beforeCol >= M) continue;
-
-                    for (int beforeDir = 0; beforeDir < 3; beforeDir++) {
-                        if (beforeDir == d || dp[i - 1][beforeCol][beforeDir] == Integer.MAX_VALUE) continue;
-                        dp[i][j][d] = Math.min(dp[i][j][d], dp[i - 1][beforeCol][beforeDir] + cost[i][j]);
-                    }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (j == 1) {
+                    dp[i][j][0] = Math.min(dp[i - 1][j][1], dp[i - 1][j + 1][2]) + cost[i][j];
+                    dp[i][j][1] = dp[i - 1][j + 1][2] + cost[i][j];
+                    dp[i][j][2] = dp[i - 1][j][1] + cost[i][j];
+                } else if (j == M) {
+                    dp[i][j][0] = dp[i - 1][j][1] + cost[i][j];
+                    dp[i][j][1] = dp[i - 1][j - 1][0] + cost[i][j];
+                    dp[i][j][2] = Math.min(dp[i - 1][j - 1][0], dp[i - 1][j][1]) + cost[i][j];
+                } else {
+                    dp[i][j][0] = Math.min(dp[i - 1][j][1], dp[i - 1][j + 1][2]) + cost[i][j];
+                    dp[i][j][1] = Math.min(dp[i - 1][j - 1][0], dp[i - 1][j + 1][2]) + cost[i][j];
+                    dp[i][j][2] = Math.min(dp[i - 1][j - 1][0], dp[i - 1][j][1]) + cost[i][j];
                 }
             }
         }
 
         int answer = Integer.MAX_VALUE;
 
-        for (int j = 0; j < M; j++)
-            for (int d = 0; d < 3; d++)
-                answer = Math.min(answer, dp[N - 1][j][d]);
+        for (int i = 1; i <= M; i++) {
+            for (int j = 0; j < 3; j++) {
+                answer = Math.min(answer, dp[N][i][j]);
+            }
+        }
 
         System.out.println(answer);
     }
