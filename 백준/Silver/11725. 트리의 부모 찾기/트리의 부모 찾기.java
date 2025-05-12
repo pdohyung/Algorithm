551469614139c97c16e036af008ea4fd066b2779
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-05-12 17:32:22 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 584 ms, Memory: 68892 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,49 +1,49 @@
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static ArrayList<Integer>[] graph;
-    static int N;
-    static boolean[] visited;
-    static int[] answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        graph = new ArrayList[N + 1];
-        visited = new boolean[N + 1];
-        answer = new int[N + 1];
+        int N = Integer.parseInt(br.readLine());
+        boolean[] visited = new boolean[N + 1];
+        int[] parent = new int[N + 1];
 
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        List<Integer>[] graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
 
         for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            graph[start].add(end);
-            graph[end].add(start);
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            graph[s].add(e);
+            graph[e].add(s);
         }
 
-        // DFS 수행
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(1);
         visited[1] = true;
-        dfs(1);
 
-        for (int i = 2; i <= N; i++) {
-            System.out.println(answer[i]);
-        }
-    }
+        while (!q.isEmpty()) {
+            int now = q.poll();
 
-    private static void dfs(int start) {
-        for (int now : graph[start]) {
-            if (!visited[now]) {
-                visited[now] = true;
-                answer[now] = start;
-                dfs(now);
+            for (int next : graph[now]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    parent[next] = now;
+                    q.offer(next);
+                }
             }
         }
+
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 2; i <= N; i++) {
+            answer.append(parent[i]).append("\n");
+        }
+
+        System.out.println(answer);
     }
 }
