85e5984a094ee9afa06e9d838b1e4fe78ebecdb0
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-05-17 18:42:58 +0900

    [Silver I] Title: 기타 레슨, Time: 264 ms, Memory: 23908 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -4,16 +4,13 @@ import java.io.*;
 
 public class Main {
 
-    static int N, M;
-    static int[] A;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        A = new int[N];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
         int start = 0;
         int end = 0;
 
@@ -21,36 +18,33 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
-            start = Math.max(start, A[i]);
             end += A[i];
+            start = Math.max(start, A[i]);
         }
 
         while (start <= end) {
             int mid = (start + end) / 2;
+            int cnt = 0;
+            int sum = 0;
 
-            int cnt = getCnt(mid);
+            for (int i = 0; i < N; i++) {
+                if (sum + A[i] > mid) {
+                    sum = 0;
+                    cnt++;
+                }
 
-            if (cnt > M) start = mid + 1;
-            else end = mid - 1;
+                sum += A[i];
+            }
+
+            if (sum > 0) cnt++;
+
+            if (cnt <= M) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
         }
 
         System.out.println(start);
-    }
-
-    private static int getCnt(int mid) {
-        int sum = 0;
-        int cnt = 0;
-
-        for (int i = 0; i < N; i++) {
-            if (sum + A[i] > mid) {
-                sum = 0;
-                cnt++;
-            }
-            sum += A[i];
-        }
-
-        if (sum != 0) cnt++;
-
-        return cnt;
     }
 }
