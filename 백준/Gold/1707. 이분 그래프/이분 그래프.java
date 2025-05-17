90958e0ee7b375d9f8856b9c4a2b205d609c55b5
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-05-18 00:35:32 +0900

    [Gold IV] Title: 이분 그래프, Time: 848 ms, Memory: 272172 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,62 +1,71 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int v, e;
+
     static ArrayList<Integer>[] graph;
     static int[] color;
-    static boolean isCycle;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
         StringBuilder answer = new StringBuilder();
 
-        for (int t = 0; t < T; t++) {
+        for (int k = 0; k < K; k++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            v = Integer.parseInt(st.nextToken());
-            e = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
 
-            graph = new ArrayList[v + 1];
-            for (int i = 1; i <= v; i++) graph[i] = new ArrayList<>();
+            color = new int[V + 1];
+            graph = new ArrayList[V + 1];
+            for (int i = 1; i <= V; i++) graph[i] = new ArrayList<>();
 
-            for (int i = 0; i < e; i++) {
+            for (int i = 0; i < E; i++) {
                 st = new StringTokenizer(br.readLine());
-                int start = Integer.parseInt(st.nextToken());
-                int end = Integer.parseInt(st.nextToken());
-                graph[start].add(end);
-                graph[end].add(start);
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                graph[s].add(e);
+                graph[e].add(s);
             }
 
-            isCycle = false;
-            color = new int[v + 1];
+            String result = "YES";
 
-            for (int i = 1; i <= v; i++) {
+            for (int i = 1; i <= V; i++) {
                 if (color[i] != 0) continue;
-                dfs(i, 1);
-            }
 
-            if (isCycle) {
-                answer.append("NO\n");
-            } else {
-                answer.append("YES\n");
+                if (bfs(i)) {
+                    result = "NO";
+                    break;
+                }
             }
+            
+            answer.append(result).append("\n");
         }
 
         System.out.println(answer);
     }
 
-    private static void dfs(int start, int c) {
-        color[start] = c;
+    static boolean bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        color[start] = 1;
 
-        for (int next : graph[start]) {
-            if (color[next] == 0) {
-                dfs(next, -c);
-            } else if (color[start] == color[next]) {
-                isCycle = true;
+        while (!q.isEmpty()) {
+            int now = q.poll();
+
+            for (int next : graph[now]) {
+                if (color[next] == 0) {
+                    color[next] = color[now] * -1;
+                    q.offer(next);
+                } else if (color[next] == color[now]) {
+                    return true;
+                }
             }
         }
+
+        return false;
     }
 }
