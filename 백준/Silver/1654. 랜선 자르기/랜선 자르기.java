f7dcf4f7d83f77c8a9f9d30f8236b0b342c78570
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-05-17 19:19:49 +0900

    [Silver II] Title: 랜선 자르기, Time: 148 ms, Memory: 17412 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,18 +1,20 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        // N개의 랜선을 만들어야 함, 자르고 나머지 길이는 버림, N개 보다 많이 만드는 것도 포함, 최대 랜선 길이
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int K = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        int[] A = new int[K];
+        long[] lines = new long[K];
 
         for (int i = 0; i < K; i++) {
-            A[i] = Integer.parseInt(br.readLine());
+            lines[i] = Long.parseLong(br.readLine());
         }
 
         long start = 1;
@@ -23,7 +25,7 @@ public class Main {
             long cnt = 0;
 
             for (int i = 0; i < K; i++) {
-                cnt += A[i] / mid;
+                cnt += lines[i] / mid;
             }
 
             if (cnt >= N) {
