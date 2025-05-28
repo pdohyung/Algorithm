d9556e68c2c8b4ceeff14e6c2e63870949ee5d5a
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-05-28 16:14:54 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 4.53 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 경주로 건설 - 67259 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259) 
 
 ### 성능 요약
 
-메모리: 76.6 MB, 시간: 9.26 ms
+메모리: 81.8 MB, 시간: 4.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 22일 01:11:01
+2025년 05월 28일 16:14:52
 
 ### 문제 설명
 
--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,66 +1,77 @@
 import java.util.*;
 
 class Node {
-    int x, y, d, c;
+    int x, y, d;
     
-    Node(int x, int y, int d, int c) {
+    Node(int x, int y, int d) {
         this.x = x;
         this.y = y;
         this.d = d;
-        this.c = c;
     }
 }
 
 class Solution {
+    
+    int N;
+    int[][] B;
+    int[][][] C;
+    
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
+    
     public int solution(int[][] board) {
-        // 어디 방향에서 도착하냐에 따라 값이 달라질 수 있음.
-        int N = board.length;
-        int M = board[0].length;
-        int[][][] C = new int[N][M][4];
+        // NxN의 정사각형 격자 형태, 0은 칸이 비어있음, 1은 벽이 채워짐
+        // 경주로를 (0,0) 부터 (N-1, N-1)까지 건설하는 최소 비용
+        // 코너 부분은 총 600원, 일반은 100원
+        N = board.length;
+        B = board;
+        C = new int[N][N][4];
         
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+            for (int j = 0; j < N; j++) {
                 Arrays.fill(C[i][j], Integer.MAX_VALUE);
             }
         }
         
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {-1, 1, 0, 0};
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(0, 0, 1, 0));
-        q.offer(new Node(0, 0, 3, 0));
         Arrays.fill(C[0][0], 0);
+        bfs();
+        
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < 4; i++) {
+            answer = Math.min(answer, C[N - 1][N - 1][i]);
+        }
+        return answer;
+    }
+    
+    void bfs() {
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(0, 0, 1)); // 아래
+        q.offer(new Node(0, 0, 3)); // 오른쪽
         
         while (!q.isEmpty()) {
             Node now = q.poll();
             int x = now.x;
             int y = now.y;
             int d = now.d;
-            int c = now.c;
             
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                int next = c;
                 
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M || board[nx][ny] == 1) continue;
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N || B[nx][ny] == 1) continue;
                 
-                if (d == i) next += 100;
-                else next += 600;
-                
-                if (C[nx][ny][i] > next) {
-                    C[nx][ny][i] = next;
-                    q.offer(new Node(nx, ny, i, next));
+                if (i == d) {
+                    if (C[nx][ny][i] > C[x][y][d] + 100) {
+                        C[nx][ny][i] = C[x][y][d] + 100;
+                        q.offer(new Node(nx, ny, i));
+                    }
+                } else {
+                    if (C[nx][ny][i] > C[x][y][d] + 600) {
+                        C[nx][ny][i] = C[x][y][d] + 600;
+                        q.offer(new Node(nx, ny, i));
+                    }
                 }
             }
         }
-        
-        int answer = Integer.MAX_VALUE;
-        
-        for (int i = 0; i < 4; i++) {
-            answer = Math.min(answer, C[N - 1][M - 1][i]);
-        }
-        
-        return answer;
     }
 }
