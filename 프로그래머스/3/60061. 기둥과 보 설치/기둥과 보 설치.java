3ccc6dca73ab4475e750ba74f98a331fba081cf6
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-05-29 12:15:30 +0900

    [level 3] Title: 기둥과 보 설치, Time: 41.18 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+class Solution {
+    
+    int N;
+    boolean[][][] M;
+    
+    public int[][] solution(int n, int[][] build_frame) {
+        // 기둥은 바닥 위에 있거나, 보의 한쪽 끝 부분 위에 있거나, 또는 다른 기둥 위에만 설치 가능
+        // 보는 한쪽 끝 부분이 기둥 위에 있거나, 양쪽 끝 부분이 다른 보와 동시에 연결돼야 설치 가능
+        // 보를 설치할 때, 양쪽 끝 부분이 다른 보와 연결되는 경우, 왼쪽 오른쪽 보가 먼저 설치돼야 함
+        // 기둥과 보를 삭제 하는 경우도 있는데, 삭제한 후 남은 기둥과 보들 또한 위 규칙을 만족해야 함
+        // 만약 작업을 수행한 결과가 조건을 만족하지 않는다면 해당 작업을 무시
+        // 구조물 설치 입력은, 기둥 보는 오른쪽, 기둥은 위쪽 방향으로 설치, 삭제 진행
+        N = n;
+        M = new boolean[N + 1][N + 1][2];
+        
+        for (int[] bf : build_frame) {
+            int x = bf[0];
+            int y = bf[1];
+            int type = bf[2]; // 0 = 기둥, 1 = 보
+            int oper = bf[3]; // 0 = 삭제, 1 = 설치
+            
+            if (oper == 1) { // 설치
+                if (canInstall(x, y, type)) {
+                    M[x][y][type] = true;
+                }
+            } else { // 삭제
+                M[x][y][type] = false;
+
+                if (!isValidAll()) {
+                    M[x][y][type] = true;
+                }
+            }
+        }
+        
+        List<int[]> result = new ArrayList<>();
+        
+        for (int x = 0; x <= N; x++) {
+            for (int y = 0; y <= N; y++) {
+                for (int t = 0; t < 2; t++) {
+                    if (M[x][y][t]) {
+                        result.add(new int[]{x, y, t});
+                    }
+                }
+            }
+        }
+        
+        result.sort((a, b) -> {
+            if (a[0] != b[0]) return a[0] - b[0];
+            else if (a[1] != b[1]) return a[1] - b[1];
+            return a[2] - b[2];
+        });
+        
+        int[][] answer = new int[result.size()][3];
+        
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+        
+        return answer;
+    }
+    
+    boolean isValidAll() {
+        for (int x = 0; x <= N; x++) {
+            for (int y = 0; y <= N; y++) {
+                for (int t = 0; t < 2; t++) {
+                    if (M[x][y][t] && !canInstall(x, y, t)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    boolean canInstall(int x, int y, int type) {
+        if (type == 0) {
+            return y == 0 || M[x][y][1] || M[x][y - 1][0] || (x > 0 && M[x - 1][y][1]);
+        } else {
+            return M[x][y - 1][0] || M[x + 1][y - 1][0] || (x > 0 && M[x - 1][y][1] && M[x + 1][y][1]);
+        }
+    }
+}
