a7e009c32b433a331232fbdbec06ce513a087fa5
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-06-01 19:23:49 +0900

    [level 3] Title: 등굣길, Time: 0.58 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,18 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
+        // 오른쪽과 아래쪽으로만 이동 가능, 최단경로의 개수를 리턴
+        int MOD = 1_000_000_007;
         int[][] dp = new int[n + 1][m + 1];
-        boolean[][] pass = new boolean[n + 1][m + 1];
+        boolean[][] P = new boolean[n + 1][m + 1];
         
         for (int[] p : puddles) {
-            pass[p[1]][p[0]] = true;
+            P[p[1]][p[0]] = true;
         }
         
-        dp[1][1] = 1;
+        dp[0][1] = 1;
         
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (pass[i][j]) continue;
-                dp[i][j] += (dp[i - 1][j] + dp[i][j - 1]) % 1_000_000_007;
+                if (P[i][j]) continue;
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
             }
         }
         
