222e88fb5628d1c7094558e005daa6a4e552b68a
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-06-01 13:34:55 +0900

    [level 2] Title: 타겟 넘버, Time: 5.85 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,29 +1,30 @@
 class Solution {
-    static int answer;
-    static int end;
-    static int targetNumber;
-    static int[] inputNumbers;
+    
+    int answer, N, T;
+    int[] A;
     
     public int solution(int[] numbers, int target) {
-        answer = 0;
-        end = numbers.length;
-        targetNumber = target;
-        inputNumbers = numbers;
+        // n개의 음이 아닌 정수들이 있고, 이 정수들을 순서를 바꾸지 않고 적절히 더하거나 빼서 타겟 넘버 만들기
+        // 거의 2^20이기 때문에 1백만 정도의 시간복잡도
+        N = numbers.length;
+        A = numbers;
+        T = target;
         
+        answer = 0;
         dfs(0, 0);
         
         return answer;
     }
     
-    private void dfs(int res, int depth) {
-        if(depth == end) {
-            if (res == targetNumber) {
-                answer++;
+    void dfs(int depth, int sum) {
+        if (depth == N) {
+            if (sum == T) {
+               answer++; 
             }
             return;
         }
         
-         dfs(res + inputNumbers[depth], depth + 1);
-         dfs(res - inputNumbers[depth], depth + 1);
+        dfs(depth + 1, sum + A[depth]);
+        dfs(depth + 1, sum - A[depth]);
     }
 }
