1908ab1b7fdef320d92a650d1a12eaa984bcd760
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-06-01 20:11:10 +0900

    [level 3] Title: 네트워크, Time: 1.01 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,37 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] computers) {
-        ArrayList<Integer>[] graph = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
-        boolean[] visited = new boolean[n + 1];
+        ArrayList<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
         
         for (int i = 0; i < n; i++) {
-            int[] nodes = computers[i];
-            
-            for (int j = 0; j < nodes.length; j++) {
-                if (i == j) continue;
-                if (nodes[j] == 1) graph[i + 1].add(j + 1);
+            for (int j = 0; j < n; j++) {
+                if (i != j && computers[i][j] == 1) {
+                    graph[i].add(j);
+                }
             }
         }
         
         int answer = 0;
+        boolean[] visited = new boolean[n];
         
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             if (visited[i]) continue;
             
-            answer++;
             visited[i] = true;
-            Queue<Integer> q = new ArrayDeque<>();
+            answer++;
+            
+            Queue<Integer> q = new LinkedList<>();
             q.offer(i);
             
             while (!q.isEmpty()) {
                 int now = q.poll();
                 
                 for (int next : graph[now]) {
-                    if (!visited[next]) {
-                        visited[next] = true;
-                        q.offer(next);
-                    }
+                    if (visited[next]) continue;
+                    visited[next] = true;
+                    q.offer(next);
                 }
             }
         }
