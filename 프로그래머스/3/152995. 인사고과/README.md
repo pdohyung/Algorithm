22b23f665dd51b7c3abbf9af4329a34fae545ff7
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-06-08 15:52:56 +0900

    [level 3] Title: 인사고과, Time: 77.13 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/README.md
+++ 프로그래머스/3/152995. 인사고과/README.md
@@ -1,10 +1,10 @@
 # [level 3] 인사고과 - 152995 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995#) 
 
 ### 성능 요약
 
-메모리: 131 MB, 시간: 72.89 ms
+메모리: 129 MB, 시간: 77.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 26일 10:15:47
+2025년 06월 08일 15:52:54
 
 ### 문제 설명
 
--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -11,24 +11,21 @@ class Score {
 
 class Solution {
     public int solution(int[][] scores) {
-        // 각 사원마다 태도 점수, 동료 평가 점수 기록
-        // 어떤 사원이 다른 임의의 사원보다 두 점수가 모두 낮은 경우가 한 번이라도 있다면, 인센티브 X
-        // 그렇지 않은 사원에 대해서는 점수 합 내림차순으로 석차를 매김, 동석차 수만큼 다음 석차는 건너 띔
-        // 완호의 점수는 0번째 배열이고, 못받는 경우는 -1을 리턴
-        int targetA = scores[0][0];
-        int targetB = scores[0][1];
-        
-        PriorityQueue<Score> pq = new PriorityQueue<>((o1, o2) -> {
-            if (o1.a != o2.a) return o2.a - o1.a;
-            return o1.b - o2.b;
+        // 각 사원은 근태, 동료 점수가 있음, 임의의 사원보다 두 점수가 모두 낮은 경우라면 인센티브를 못받음
+        // 그렇지 않은 사원들에 대해서 두 점수의 합이 높은 순서대로 석차를 내며 차등 지급
+        // 합이 동일하면 동석차, 동석차의 수만큼 다음 석차는 건너뜀
+        int ta = scores[0][0];
+        int tb = scores[0][1];
+        PriorityQueue<Score> pq = new PriorityQueue<>((a, b) -> {
+           if (a.a != b.a) return b.a - a.a;
+            return a.b - b.b;
         });
         
-        for (int[] s : scores) {
-            pq.offer(new Score(s[0], s[1]));
+        for (int[] score : scores) {
+            pq.offer(new Score(score[0], score[1]));
         }
         
         List<Score> result = new ArrayList<>();
-        
         int maxB = 0;
         
         while (!pq.isEmpty()) {
@@ -36,15 +33,13 @@ class Solution {
             int a = now.a;
             int b = now.b;
             
-            // if (a > targetA && b > targetB) return -1;
-            
             if (b >= maxB) {
-                result.add(now);
                 maxB = b;
+                result.add(now);
             }
         }
         
-        Collections.sort(result, (o1, o2) -> (o2.a + o2.b) - (o1.a + o1.b));
+        result.sort((a, b) -> (b.a + b.b) - (a.a + a.b));
         
         int answer = -1;
         int rank = 0;
@@ -52,13 +47,15 @@ class Solution {
         
         for (int i = 0; i < result.size(); i++) {
             Score now = result.get(i);
+            int a = now.a;
+            int b = now.b;
             
-            if (now.a + now.b < sum) {
+            if (a + b < sum) {
+                sum = a + b;
                 rank = i + 1;
-                sum = now.a + now.b;
             }
             
-            if (targetA == now.a && targetB == now.b) {
+            if (a == ta && b == tb) {
                 answer = rank;
                 break;
             }
