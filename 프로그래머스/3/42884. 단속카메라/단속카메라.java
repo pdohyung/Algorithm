ef98f7c6ece004fa31acb70e953f433414cca809
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-06-13 21:29:56 +0900

    [level 3] Title: 단속카메라, Time: 14.37 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,21 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+        int answer = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         for (int[] r : routes) pq.offer(r);
         
-        int cnt = 1;
-        int last = pq.peek()[1];
+        int before = Integer.MIN_VALUE;
         
         while (!pq.isEmpty()) {
             int[] now = pq.poll();
             
-            if (now[0] > last) {
-                cnt++;
-                last = now[1];
-            } 
+            if (now[0] > before) {
+                answer++;
+                before = now[1];
+            }
         }
         
-        return cnt;
+        return answer;
     }
 }
