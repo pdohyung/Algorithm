ec1ba26fb274a9f3a857c85c2b825ea1798c63b7
pdohyung <58456758+pdohyung@users.noreply.github.com>
2025-06-26 18:44:44 +0900

    [Gold IV] Title: 키 순서, Time: 596 ms, Memory: 36944 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        boolean[][] graph = new boolean[N + 1][N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            graph[s][e] = true;
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (graph[i][k] && graph[k][j]) {
+                        graph[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+
+        for (int i = 1; i <= N; i++) {
+            int cnt = 0;
+
+            for (int j = 1; j <= N; j++) {
+                if (i == j) continue;
+                if (graph[i][j] || graph[j][i]) cnt++;
+            }
+
+            if (cnt == N - 1) answer++;
+        }
+
+        System.out.println(answer);
+    }
+}
