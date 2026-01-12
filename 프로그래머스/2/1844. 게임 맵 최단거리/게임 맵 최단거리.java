197a0154e7196ca8cc860f7e93f8d3a3834b7f3d
pdohyung <58456758+pdohyung@users.noreply.github.com>
2026-01-12 23:01:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.44 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,9 +1,10 @@
 import java.util.*;
+import java.io.*;
 
 class Node {
     int x, y;
     
-    Node (int x, int y) {
+    Node(int x, int y) {
         this.x = x;
         this.y = y;
     }
@@ -11,49 +12,39 @@ class Node {
 
 class Solution {
     
-    int N, M;
-    int[][] B;
-    int[] dx = {0, 0, -1, 1};
-    int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, -1, 0, 1};
+    static int[] dy = {-1, 0, 1, 0};
+    static int N, M;
+    static int[][] result, map;
+    static boolean[][] visited;
     
     public int solution(int[][] maps) {
-        // (1, 1)에서 (N, M)까지 최단거리 구하기
+        map = maps;
         N = maps.length;
         M = maps[0].length;
-        B = new int[N][M];
-        
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (maps[i][j] == 0) {
-                    B[i][j] = -1;
-                } else {
-                    B[i][j] = 0;
-                }
-            }
-        }
-        
+        result = new int[N][M];
+        visited = new boolean[N][M];
         bfs(new Node(0, 0));
-        
-        return B[N - 1][M - 1] == 0 ? -1 : B[N - 1][M - 1];
+        if (result[N - 1][M - 1] == 0) return -1;
+        else return result[N - 1][M - 1];
     }
-    
-    void bfs(Node start) {
-        Queue<Node> q = new LinkedList<>();
+
+    static void bfs(Node start) {
+        Queue<Node> q = new ArrayDeque<>();
         q.offer(start);
-        B[start.x][start.y] = 1;
-        
-        while (!q.isEmpty()) {
+        visited[start.x][start.y] = true;
+        result[start.x][start.y] = 1;
+
+        while(!q.isEmpty()) {
             Node now = q.poll();
             int x = now.x;
             int y = now.y;
-            
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M || B[nx][ny] != 0) continue;
-                
-                B[nx][ny] = B[x][y] + 1;
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                if (visited[nx][ny] || result[nx][ny] != 0 || map[nx][ny] == 0) continue;
+                result[nx][ny] = result[x][y] + 1;
                 q.offer(new Node(nx, ny));
             }
         }
