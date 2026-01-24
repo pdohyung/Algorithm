479f6309925f399f162d3e22347e2aed85462b99
pdohyung <58456758+pdohyung@users.noreply.github.com>
2026-01-25 00:29:04 +0900

    [Gold IV] Title: 좋다, Time: 144 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,49 +1,44 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
-    static long[] numbers;
+
+    static int[] num;
+    static int N, answer;
 
     public static void main(String[] args) throws IOException {
-        new Main().solution();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        num = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) num[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(num);
+        answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            find(num[i], i);
+        }
+
+        System.out.println(answer);
     }
 
-    void solution() throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        numbers = new long[n];
-        st = new StringTokenizer(br.readLine());
+    private static void find(int target, int i) {
+        int left = 0;
+        int right = num.length - 1;
 
-        for (int i = 0; i < n; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(numbers);
-        int startIdx = 0;
-        int endIdx = 0;
-        int ans = 0;
-        long sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            startIdx = 0;
-            endIdx = n - 1;
-            while (endIdx > startIdx) {
-                sum = numbers[startIdx] + numbers[endIdx];
-                if (sum < numbers[i] || i == startIdx) {
-                    startIdx++;
-                    continue;
-                }
-                if (sum > numbers[i] || i == endIdx) {
-                    endIdx--;
-                    continue;
-                }
-                if (sum == numbers[i]) {
-                    ans++;
-                    break;
-                }
+        while (left < right) {
+            int now = num[left] + num[right];
+            if (now == target && left != i && right != i) {
+                answer++;
+                return;
+            } else if (now > target) {
+                right--;
+            } else if (now < target) {
+                left++;
+            } else {
+                if (left == i) left++;
+                else right--;
             }
         }
-        System.out.println(ans);
     }
 }
